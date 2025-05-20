7a7e226288789997a6f32466279477b2fff1a0a9
DavidAkerlind <david.akerlind@icloud.com>
2025-05-20 11:25:08 +0200

    feat: add CORS middleware to handle allowed origins for API requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import menuRouter from './routes/menuRouter.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 // Config
 dotenv.config();
@@ -9,9 +10,27 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_STRING);
 const database = mongoose.connection;
+const allowedOrigins = [
+	'https://www.davidakerlind.com', // Production URL
+	'http://localhost:7000', // Local dev URL
+];
 
 // Middlewares
 app.use(express.json());
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// Om det är en direkt curl / Postman / serveranrop utan origin, tillåt
+			if (!origin) return callback(null, true);
+
+			if (allowedOrigins.includes(origin)) {
+				return callback(null, true);
+			} else {
+				return callback(new Error('Not allowed by CORS'));
+			}
+		},
+	})
+);
 
 // Routes
 app.use('/api/menus', menuRouter);
